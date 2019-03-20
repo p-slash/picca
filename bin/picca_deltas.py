a0c99e68270a98c2347155ce04f7a4837dff94c2
Michael Walther <michael.walther@cea.fr>
2019-03-20 16:41:50 +0100

    replace mean pixel size by median pixel size to avoid being sensitive to outliers (from masking)

--- bin/picca_deltas.py
+++ bin/picca_deltas.py
@@ -408,7 +408,7 @@ if __name__ == '__main__':
                 desi_pixsize=1 #set desi pixel size to one angstrom, generalize later
                 if (args.mode=='desi') :
                     #dll = (d.ll[-1]-d.ll[0])/float(len(d.ll)-1)  #this is not the right number given that pixelization is changed at spectra readin
-                    dll=sp.mean(10**-d.ll)*desi_pixsize #this is 1 angstrom pixel size * mean(1/lambda)
+                    dll=sp.median(10**-d.ll)*desi_pixsize #this is 1 angstrom pixel size * mean(1/lambda)
 
                 d.mean_reso*=constants.speed_light/1000.*dll*sp.log(10.0)
 
