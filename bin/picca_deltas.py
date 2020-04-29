c68194158ffe57ed99d12d0b596028a201818800
Michael Walther <michael.walther@cea.fr>
2020-04-29 18:45:54 +0200

    add unit

--- bin/picca_deltas.py
+++ bin/picca_deltas.py
@@ -527,7 +527,7 @@ if __name__ == '__main__':
 
                     cols=[d.ll,d.de,d.iv,diff,d.co]
                     names=['LOGLAM','DELTA','IVAR','DIFF','CONT']
-                    units=['log Angstrom','','','']
+                    units=['log Angstrom','','','','']
                     comments = ['Log lambda','Delta field','Inverse variance','Difference','Continuum']
                     if args.use_resolution_matrix and (args.mode is not None and 'desi' in args.mode) :
                         cols.extend([resomat])
