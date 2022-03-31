fix error for diff calculation

py/picca/io.py
@@ -1570,11 +1570,11 @@ def read_from_desi(nside,in_dir,thid,ra,dec,zqso,plate,mjd,fid,order,pk1d=None,m
                     diff_sp = (iv*tspecData['DIFF'][wt]).sum(axis=0)
                     w = iv.sum(axis=0)>0.
                     diff_sp[w] /= iv[w]
-                elif(coadd_by_picca&compute_diff_flux):
+                elif(coadd_by_picca&(compute_diff_flux != "None")):
                     diff_sp = exp_diff_desi(tspecData,wt,compute_diff_flux)
                     if(diff_sp is None):
                         continue
-                elif((not coadd_by_picca)&compute_diff_flux):
+                elif((not coadd_by_picca)&(compute_diff_flux != "None")):
                     print("Option coadd_by_picca need to be used when DIFF is not pre-computed in the coadd files")
                     diff_sp = None
                 else: